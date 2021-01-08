f60600e6191cd0093cbc2d3722c7c47600e92e5e
Ben Reeves <benwolverine2019@gmail.com>
2021-01-07 19:57:24 -0600

    [JNTUB-HW] Finish v0.1 of front panel

--- hardware/gerber.sh
+++ hardware/gerber.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-BUILD_DIR=finalize
-GERBER_DIR=gerber
-
 zip_files() {
-	SUBDIR_NAME=$1
-	mkdir $GERBER_DIR/$SUBDIR_NAME
-	cp $BUILD_DIR/$SUBDIR_NAME/*.{gbr,drl} $GERBER_DIR/$SUBDIR_NAME/
-	pushd $GERBER_DIR/$SUBDIR_NAME
-	zip $SUBDIR_NAME.zip *.{gbr,drl}
+	SRC_DIR=$1
+	DST_DIR=$2
+	SUBDIR_NAME=$(basename $DST_DIR)
+	mkdir $DST_DIR
+	cp $SRC_DIR/*.{gbr,drl} $DST_DIR/
+	pushd $DST_DIR
+	zip JNTUB_$SUBDIR_NAME.zip *.{gbr,drl}
 	popd
 }
 
 set -x
-rm -rf $GERBER_DIR/*
-zip_files board1
-zip_files board2
+rm -rf gerber/*
+zip_files finalize/board1 gerber/board1
+zip_files finalize/board2 gerber/board2
+zip_files front-panel/gerber gerber/panel
